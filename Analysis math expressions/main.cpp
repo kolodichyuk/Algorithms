dbb6fd585239141100d3a58bc8bd31c3b4de774d
Dima <40443779+kolodichyuk@users.noreply.github.com>
2019-01-29 12:57:17 +0200

    entry point

--- Analysis math expressions/main.cpp
+++ Analysis math expressions/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main() {
 	Analysis* analysis = new Analysis();
-	cout << "2+2*2 = "                 << analysis->Calculation("2+2*2") 
+	cout << "2+2*2 = "                       << analysis->Calculation("2+2*2") 
 		   << "\n3.5*2+3 = "             << analysis->Calculation("3.5*2+3") 
 		   << "\n(5*6 - 3) + 3^(2*2) = " << analysis->Calculation("(5*6 - 3) + 3^(9%5)")
 		   << "\n4.562*2 + 3 - 5/2 + (3*2 + 5)*6.3/2 + 5*(25 - 4)*2^5 = " 
