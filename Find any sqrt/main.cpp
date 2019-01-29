d7a1e1e152fda92f2c4c4f80a550e83a4f1f2c11
Dima <40443779+kolodichyuk@users.noreply.github.com>
2019-01-29 13:14:09 +0200

    find any sqrt

--- Find any sqrt/main.cpp
+++ Find any sqrt/main.cpp
@@ -4,12 +4,12 @@ double FAS(double);
 
 using namespace std;
 int main() {
-	cout << "Sqrt(25): " << FAS(25) << "\n"
-		   << "Sqrt(11): "  << FAS(11) << "\n"
-		   << "Sqrt(31): "  << FAS(31) << "\n"
-		   << "Sqrt(46): "  << FAS(46) << "\n"
-		   << "Sqrt(64): "  << FAS(64) << "\n"
-		   << "Sqrt(72): "  << FAS(72) << "\n";
+	cout << "Sqrt(25): "  << FAS(25) << "\n"
+             << "Sqrt(11): "  << FAS(11) << "\n"
+	     << "Sqrt(31): "  << FAS(31) << "\n"
+	     << "Sqrt(46): "  << FAS(46) << "\n"
+             << "Sqrt(64): "  << FAS(64) << "\n"
+	     << "Sqrt(72): "  << FAS(72) << "\n";
 	_getch();
 	return 0;
 }
